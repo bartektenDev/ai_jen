0105b00ea90f09f8713142a88f9b4998dcf1f1db
bartektenDev <bartek8991@live.com>
2019-07-18 09:40:00 -0500

    Build base script in python

---
+++ run_ai.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+# Import libraries
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from pyautogui import press, typewrite, hotkey
+
+import subprocess as sp
+import requests
+import time
+import re
+
+#DEFINE
+targetDatabaseURL = 'https://www.amazon.com'
+
+
+def initializeJEN():
+    global userSearchItemName, userSearchIntensity
+    #Run AI
+    print('BOOTING JEN...')
+
+    time.sleep(3)
+    # WINDOWS tmp = sp.call('cls',shell=True)
+    # LINUX tmp = sp.call('clear',shell=True)
+    tmp = sp.call('cls',shell=True)
+    print("Jen is ready.")
+
+    #Ask user for target item to look for
+    userSearchItemName = input("Please enter the item you would like to search: ")
+    pickItem()
+
+
+def pickItem():
+    global userSearchItemName, userSearchIntensity
+    #Select AI search intensity
+    print("Please enter intensity of search on scale 1 - 3")
+    print("1 - Quick Light Scan")
+    print("2 - Sherlock Holmes Scan")
+    print("3 - ENDOME SCAN (Intelligence beyound human knowledge)")
+
+    userSearchIntensity = input("Selection: ")
+
+    #Validate Entered Selection to be an Int and a valid option
+
+    if userSearchIntensity.startswith('1'):
+        print("You chose Quick Light Scan")
+        time.sleep(2)
+        #We passed the checkpoints to lets start searching
+        executeJENSCAN1()
+    elif userSearchIntensity.startswith('2'):
+        print("You chose Sherlock Holmes Scan")
+    elif userSearchIntensity.startswith('3'):
+        print("You chose ENDOME SCAN (Intelligence beyound human knowledge)")
+    else:
+        print("You idiot.")
+        shutdown()
+
+
+def executeJENSCAN1():
+    global userSearchItemName, userSearchIntensity, numofitems, numofgucciprice
+    tmp = sp.call('cls',shell=True)
+    print("Running...")
+
+    #Execute Scan
+    driver = webdriver.Chrome()
+    #Load page
+    driver.get(targetDatabaseURL)
+    #Page Loaded, click on  the search box
+    element = driver.find_element_by_xpath("//input[@id='twotabsearchtextbox']")
+    element.click()
+
+    time.sleep(2)
+
+    #Enter item to search using fake paste
+    typewrite(userSearchItemName)
+    time.sleep(2)
+
+    numofitems = 0
+    numofgucciprice = 0
+
+    #Enter search box
+    element = driver.find_element_by_xpath("//input[@value='Go']")
+    element.click()
+
+    time.sleep(1)
+
+    #Run through ever price and find the best option based on lowest legit price and good ratio reviews
+
+    listOfItems = driver.find_element_by_xpath("//div[@data-cel-widget='search_result_0']")
+    for everyItem in listOfItems:
+        numofitems += 1
+        scannedItemName = driver.find_element_by_xpath("//span[@class='a-size-medium a-color-base a-text-normal']")
+        vare = scannedItemName.get_attribute('innerHTML')
+        time.sleep(0.2)
+        #newvare = vare.replace(".", "")
+        print("Product:", vare)
+
+    # listOfItemsStars = driver.find_elements_by_class_name('a-icon-alt')
+    # for everyRating in listOfItemsStars:
+    #     vare = everyRating.get_attribute('innerHTML')
+    #     time.sleep(0.2)
+    #     #newvare = vare.replace("", "")
+    #     print("Rating: " + vare + "")
+
+
+    time.sleep(2)
+    print("Jen has finished scanning!")
+    time.sleep(2)
+    print("Scanned", numofitems,"products. Found ",numofgucciprice,"with a great price. Check log file `dropLIST.txt`")
+    print("Shutting down in 3 seconds..")
+    time.sleep(3)
+    #Close browser. We are done searching.
+    driver.quit()
+
+def shutdown():
+    exit()
+
+
+initializeJEN()
